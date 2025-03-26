1550a68792ce7a6c652470bca2e6ca0f32709d01
Funnygifdan <howardd3730@yahoo.com>
2025-03-26 03:57:12 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,9 +4,14 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* Your other content goes here */}
+      <h2>Welcome Minions</h2>
+      <h3>
+        This site is dedicated to my daughters Alycia, Aubree, and Hadley. 
+        I love you so much and hope you know that no matter what, I am your father.
+      </h3>
     </div>
   );
 }
 
 export default App;
+
